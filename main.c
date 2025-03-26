7dc36b22676e18a6d088f73c62acc6c4a5a440f3
Tic <enzodormeux@gmail.com>
2025-03-26 16:49:22 +0100

    First commit with all the project

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct{
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+}t_bmp8;
+
---
+++ t_bmp8.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+t_bmp8 * bmp8_loadImage(const char * filename){
+    
+}
